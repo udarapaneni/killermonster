72b8ed6c63b21d58ee1e069748a8da3a4decf59c
Umakanth Darapaneni <umakanth158@gmail.com>
2021-02-11 08:44:05 +0530

    attack monster and player damage functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const attackValue = 10;
+const monsterAttactValue = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,15 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(attackValue);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(monsterAttactValue);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You won!');
+  } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
+    alert('You Lost');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('Its a draw');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
